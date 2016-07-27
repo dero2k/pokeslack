5b7a411be29520bd632d6e87964305b82c52835b
dero2k <dergelm@me.com>
2016-07-27 03:18:18 +0200

    Update pokeslack.py

--- pokeslack.py
+++ pokeslack.py
@@ -43,12 +43,13 @@ class Pokeslack:
         from_lure = ', from a lure' if pokemon.get('from_lure', False) else ''
         miles_away = '{:.3f}'.format(distance)
 
-        pokedex_url = 'http://www.pokemon.com/us/pokedex/%s' % pokemon['pokemon_id']
+        pokedex_url = 'http://www.pokemon.com/de/pokedex/%s' % pokemon['pokemon_id']
         map_url = 'http://maps.google.com?saddr=%s,%s&daddr=%s,%s&directionsmode=walking' % (position[0], position[1], pokemon['latitude'], pokemon['longitude'])
         min_remaining = int(expires_in.total_seconds() / 60)
         time_remaining = '%s%ss' % ('%dm' % min_remaining if min_remaining > 0 else '', expires_in.seconds - 60 * min_remaining)
         stars = ''.join([':star:' for x in xrange(rarity)])
-        message = 'I found a <%s|%s> %s <%s|%s miles away> expiring in %s%s' % (pokedex_url, pokemon['name'], stars, map_url, miles_away, time_remaining, from_lure)
+        m_away = miles_away * 1609.34
+        message = '<%s|%s> %s gefunden <%s|%s meter entfernt> verschwindet in %s%s' % (pokedex_url, pokemon['name'], stars, map_url, m_away, time_remaining, from_lure)
         # bold message if rarity > 4
         if rarity >= 4:
             message = '*%s*' % message
